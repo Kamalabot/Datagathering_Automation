a95dd68961c3c75bc2991749c61da0fc8a9693c2
Kamalraj M M <kamaljp@gmail.com>
2022-02-08 14:33:13 +0530

    Adding multitype files, and jupyternotes to repo

---
+++ master-sql/Schema1-- SQLite.sql
@@ -0,0 +1,10 @@
+-- SQLite
+CREATE TABLE EMP
+       (EMPNO integer NOT NULL,
+        ENAME VARCHAR(10),
+        JOB VARCHAR(9),
+        MGR integer,
+        HIREDATE DATE,
+        SAL integer,
+        COMM integer,
+        DEPTNO integer)
